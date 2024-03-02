9168b81a0bbdb759cceda4a40e6300b3fb992076
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-02 17:07:03 +0530

    2.3.24

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -2,6 +2,7 @@ import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/widgets/thick_container.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../utils/app_styles.dart';
 
@@ -37,19 +38,37 @@ class TicketView extends StatelessWidget {
                       ),
                       Expanded(child: Container()),
                       ThickContainer(),
-                      Expanded(child: SizedBox(
-                        height: 24,
-                        child: LayoutBuilder(
-                          builder: (BuildContext context, BoxConstraints constraints) {
-                            return Flex(
-                              direction: Axis.horizontal,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: List.generate(5, (index) => Text(
-                                "-"
-                              )),
-                            );
-                          },
+                      Expanded(child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                            builder: (BuildContext context, BoxConstraints constraints) {
+                              print("The width is ${constraints.constrainWidth()}");
+                              return Flex(
+                                direction: Axis.horizontal,
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.max,
+                                children: List.generate((constraints.constrainWidth()/6).floor(), (index) => const SizedBox(
+                                  width: 3,
+                                  height: 1,
+                                  child: DecoratedBox(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                )),
+                              );
+                            },
+                          ),
                         ),
+                          Center(
+                            child: Transform.rotate(
+                              angle: 1.5,
+                              child: Icon(Icons.local_airport_rounded, color: Colors.white),
+                            ),
+                          ),
+                        ],
                       )),
                       ThickContainer(),
                       Expanded(child: Container()),
