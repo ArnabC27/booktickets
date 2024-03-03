0d9e6a4b7e886d81e92601433411b72baca69f88
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-04 03:14:51 +0530

    4.3.24

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -71,7 +71,74 @@ class SearchScreen extends StatelessWidget {
           ),
           const Gap(40),
           const AppDoubleTextWidget(bigText: "Upcoming Flights", smallText: "View All"),
-
+          const Gap(15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 400,
+                width: size.width * 0.42,
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 17),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade200,
+                      blurRadius: 1,
+                      spreadRadius: 1,
+                    )
+                  ]
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 190,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: const DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/sit.jpg"),
+                        )
+                      ),
+                    ),
+                    const Gap(12),
+                    Text(
+                      "20% discount on the early booking of the tickets. Don't miss out.",
+                      style: Styles.headLineStyle2,
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: size.width * 0.44,
+                    height: 205,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF3AB8B8),
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Discount\nfor survey",
+                          style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+                        ),
+                        const Gap(10),
+                        Text(
+                          "Take the survey about our services and get discount",
+                          style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.w500, color: Colors.white, fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
