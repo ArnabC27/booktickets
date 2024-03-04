369145b392d7e81677a2c9ea377623ca887a3f5b
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2024-03-04 23:49:14 +0530

    4.3.24

---
+++ lib/widgets/ticket_tabs.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/app_layout.dart';
+
+class AppTicketTabs extends StatelessWidget {
+  const AppTicketTabs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return FittedBox(
+      child: Container(
+        padding: const EdgeInsets.all(3.5),
+        child: Row(
+          children: [
+            Container(
+              width: size.width * 0.44,
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.horizontal(left: Radius.circular(50),),
+                color: Colors.white,
+              ),
+              child: const Center(
+                  child: Text("Airline Tickets",)
+              ),
+            ),
+            Container(
+              width: size.width * 0.44,
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.horizontal(right: Radius.circular(50),),
+                color: Colors.transparent,
+              ),
+              child: const Center(
+                  child: Text("Hotels",)
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
